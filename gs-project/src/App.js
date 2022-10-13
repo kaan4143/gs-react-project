7e3c525214f856a8c100fbbe223e97a70f819813
tuğrul kaan kılıç <tkaankilic@gmail.com>
2022-10-13 14:56:17 +0300

    gs projeckt

--- gs-project/src/App.js
+++ gs-project/src/App.js
@@ -1,16 +1,13 @@
 import "./App.scss";
-import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
-import Data from "./data.js";
+import data from "./data.js";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Main data={data} />
-    
-      <Footer/>
     </div>
   );
 }
--- gs-project/src/components/Main/Main.jsx
+++ gs-project/src/components/Main/Main.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import "./Main.scss"
 
-const Main = () => {
+const Main = ({data}) => {
   return (
-    <div>Main</div>
+    <div className='card-container'>
+      {data.map((item)=>{
+        const {id, title, desc, image}=item
+        return (
+          <div className='cards'>
+            <div className='title'>
+              <h1>{title}</h1>
+            </div>
+            <img src={image} alt="img" />
+            <div className='card-over'>
+              <p>{desc}</p>
+            </div>
+
+          </div>
+
+
+        )
+      })
+
+      }
+
+    </div>
   )
 }
 
